7492be441a781eb00d153568b9a673dc4bda98d5
Kevin Hankens <email@kevinhankens.com>
2011-04-05 08:35:04 -0400

    Protect against undefined content type

--- core/crud.js
+++ core/crud.js
@@ -1,13 +1,22 @@
 exports.routes = {
   'GET:/create/%type': {
     callback: function(req, res) {
-      var def = req.wolfpack.content_types[req.wolfpack.args.type].form;
-      var form_create = new form.engine(def);
+      // Look up a content type form definition. Should be part of a
+      // wolfpack module export. e.g. exports.content_types.form.
+      if (typeof req.wolfpack.content_types[req.wolfpack.args.type] != 'undefined') {
+        var def = req.wolfpack.content_types[req.wolfpack.args.type].form;
+        var form_create = new form.engine(def);
+        var message = form_create.renderForm();
+      }
+      else {
+        // @todo 404
+        var message = 'Content type not found';
+      }
 
       return {
         file: 'views/home.jade',
         locals: {
-          message: form_create.renderForm(),
+          message: message,
         }
       }
     }
